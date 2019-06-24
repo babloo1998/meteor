c6ea184d732afbcbf7a712e8e55f062a651ea8c5
babloo <babloo.mishra@tothenew.com>
2019-06-24 14:49:27 +0530

    Updates

---
+++ precache-manifest.892cd88cc9be13d75926adfc863c345d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f232d4eb9943dbd301ee0d805d8bd7",
+    "url": "/meteor/index.html"
+  },
+  {
+    "revision": "864c5f189bea0ca72b25",
+    "url": "/meteor/static/css/main.720fdd64.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/meteor/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "864c5f189bea0ca72b25",
+    "url": "/meteor/static/js/main.b6d9d9e5.chunk.js"
+  },
+  {
+    "revision": "1bd1d9e42a990a3a361f",
+    "url": "/meteor/static/js/runtime~main.c3248b4c.js"
+  }
+]);
